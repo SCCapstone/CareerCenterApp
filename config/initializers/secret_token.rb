30bc743729c12fa56b61f4100664a0145ee8bda3
Corey Finley <finley.corey@gmail.com>
2013-09-10 10:22:13 -0400

    Initial Commit After App Generation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CareerCenterApp::Application.config.secret_key_base = '75f17f58cee5779725e8478c2f123365fb615f91749ae556522a21a6efdd60bd49b5b750a73789436d2ff8ba3cde59c748d8fdbd8c1f1420bd0bc0e5600f6711'
